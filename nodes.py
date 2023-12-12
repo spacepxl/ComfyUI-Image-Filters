ff6003c8949ed40731ba7a8c59afe003feadf2f4
spacepxl <143970342+spacepxl@users.noreply.github.com>
2023-12-12 01:00:28 -0500

    Add files via upload

--- nodes.py
+++ nodes.py
@@ -24,15 +24,15 @@ class EnhanceDetail:
                     "step": 1
                 }),
                 "sigma": ("FLOAT", {
-                    "default": 0.01,
+                    "default": 0.1,
                     "min": 0.01,
-                    "max": 10.0,
+                    "max": 100.0,
                     "step": 0.01
                 }),
                 "denoise": ("FLOAT", {
-                    "default": 0.01,
+                    "default": 0.1,
                     "min": 0.0,
-                    "max": 1.0,
+                    "max": 10.0,
                     "step": 0.01
                 }),
                 "detail_mult": ("FLOAT", {
@@ -55,6 +55,8 @@ class EnhanceDetail:
             return (images,)
         
         d = filter_radius * 2 + 1
+        s = sigma / 10
+        n = denoise / 10
         
         dup = copy.deepcopy(images.cpu().numpy())
         
@@ -62,22 +64,108 @@ class EnhanceDetail:
             image = dup[i]
             imgB = image
             if denoise>0.0:
-                imgB = cv2.bilateralFilter(image, d, denoise, d)
+                imgB = cv2.bilateralFilter(image, d, n, d)
             
-            imgG = guidedFilter(image, image, d, sigma)
+            imgG = guidedFilter(image, image, d, s)
             
             details = (imgB/imgG - 1) * detail_mult + 1
             dup[i] = details*imgG - imgB + image
         
         return (torch.from_numpy(dup),)
 
+class GuidedFilterAlpha:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "alpha": ("IMAGE",),
+                "filter_radius": ("INT", {
+                    "default": 8,
+                    "min": 1,
+                    "max": 64,
+                    "step": 1
+                }),
+                "sigma": ("FLOAT", {
+                    "default": 0.1,
+                    "min": 0.01,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "guided_filter_alpha"
+
+    CATEGORY = "image/filters"
+
+    def guided_filter_alpha(self, image: torch.Tensor, alpha: torch.Tensor, filter_radius: int, sigma: float):
+        
+        d = filter_radius * 2 + 1
+        s = sigma / 10
+        
+        i_dup = copy.deepcopy(image.cpu().numpy())
+        a_dup = copy.deepcopy(alpha.cpu().numpy())
+        
+        for i in range(len(i_dup)):
+            image_work = i_dup[i]
+            alpha_work = a_dup[i]
+            i_dup[i] = guidedFilter(image_work, alpha_work, d, s)
+        
+        return (torch.from_numpy(i_dup),)
+
+class RemapRange:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "blackpoint": ("FLOAT", {
+                    "default": 0.0,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+                "whitepoint": ("FLOAT", {
+                    "default": 1.0,
+                    "min": 0.01,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "remap"
+
+    CATEGORY = "image/filters"
+
+    def remap(self, image: torch.Tensor, blackpoint: float, whitepoint: float):
+        
+        bp = min(blackpoint, whitepoint - 0.001)
+        scale = 1 / (whitepoint - bp)
+        
+        i_dup = copy.deepcopy(image.cpu().numpy())
+        i_dup = np.clip((i_dup - bp) * scale, 0.0, 1.0)
+        
+        return (torch.from_numpy(i_dup),)
+
 
 NODE_CLASS_MAPPINGS = {
-    #"SaveTiff": SaveTiff,
     "EnhanceDetail": EnhanceDetail,
+    "GuidedFilterAlpha": GuidedFilterAlpha,
+    "RemapRange": RemapRange,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    #"SaveTiff": "Save Tiff",
     "EnhanceDetail": "Enhance Detail",
+    "GuidedFilterAlpha": "Guided Filter Alpha",
+    "RemapRange": "Remap Range",
 }
